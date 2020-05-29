9a22ca0842f79eac3d1906c6e729c241e4bf7dc3
avdasilvab25 <avdasilvab.17@est.ucab.edu.ve>
2020-05-29 17:23:56 -0400

    Ajustes en el cliente http

--- buhocenter-frontend/src/http/http-client.ts
+++ buhocenter-frontend/src/http/http-client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Modificar el baseURL con las variables de ambiente
 const httpcustomer: AxiosInstance = axios.create({
-    baseURL: `${process.env.VUE_APP_BUHOCENTER_URL}:${process.env.VUE_APP_BUHOCENTER_PORT}/api/v1`,
+    baseURL: `http://localhost:3000/api/v1`,
     timeout: 60000,
 });
 
