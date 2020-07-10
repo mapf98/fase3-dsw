443e36b39b7745cba0ba55ea8c7fc6e47e6ad4d6
angelsucasas <44983658+angelsucasas@users.noreply.github.com>
2020-07-10 10:10:02 -0500

    FASE2-PDF-FIX: arreglando descarga de pdf en FE (#85)

--- buhocenter-frontend/src/modules/client/customers/repositories/customers.repository.ts
+++ buhocenter-frontend/src/modules/client/customers/repositories/customers.repository.ts
@@ -9,7 +9,7 @@ class CustomersRepository extends HttpRepository {
 
     public async downloadFile(id: number): Promise<boolean> {
         const response = await this.get(this.createUri([`products/facture/view/${id}`]));
-        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const url = window.URL.createObjectURL(new Blob([response]));
         const link = document.createElement('a');
         link.href = url;
         link.target = '_blank';
